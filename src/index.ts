158c247326a5ae72ce421fd81409d77993402072
Andres Osorio <andres.osoriogr@gmail.com>
2023-08-25 15:57:39 -0500

    express basics

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("hello world");
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicDirectoryPath = path.join(__dirname, "./public");
+
+app.use((req, res, next) => {
+    const { method, path } = req;
+    console.log(
+        `New request to: ${method} ${path} at ${new Date().toISOString()}`
+    );
+    console.log(res.status);
+    next();
+});
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", (req, res) => {
+    console.log(req.url);
+    res.send("Hello from the server!");
+});
+
+app.get("/:name", (req, res) => {
+    console.log(req.url);
+    res.send(`Welcome to Express Recipes, ${req.params.name}!`);
+});
+
+const port: string | number = process.env["PORT"] || 8080;
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
